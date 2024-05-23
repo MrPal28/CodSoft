c99873eb9c4aafeb0e6ba4af421d3c4815434d6f
Arindam Pal <143476726+MrPal28@users.noreply.github.com>
2024-05-23 19:03:33 +0530

    Delete GenerateNumber.java

--- GenerateNumber.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-public class GenerateNumber {
-    // Random generate method normal level
-    public int Generate() {
-        Random num = new Random();
-        int number = num.nextInt(100);
-        return number;
-    }
-}
